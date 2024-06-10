a742a833b19ad7065a473eabfb60afe2dac8c3e4
malasanivenkataamarnathSS <mvenkataamarnath@gmail.com>
2024-06-10 10:53:07 +0530

    using these shell script we can push docker images to docker Hub

---
+++ dockerimages-pushTodockerHub.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker push amarnathvenkatam/spring-petclinic-config-server:dev
+docker push amarnathvenkatam/spring-petclinic-discovery-server:dev
+docker push amarnathvenkatam/spring-petclinic-api-gateway:dev
+docker push amarnathvenkatam/spring-petclinic-visits-service:dev
+docker push amarnathvenkatam/spring-petclinic-vets-service:dev
+docker push amarnathvenkatam/spring-petclinic-customers-service:dev
+docker push amarnathvenkatam/spring-petclinic-admin-server:dev
